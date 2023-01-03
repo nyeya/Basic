44281456b2b5fcb1a8d204dec24aca60bc3c3953
nyeya <muhammadkamaldeen92@gmail.com>
2023-01-02 16:33:31 -0800

    Added Pattern Programming Directory

---
+++ Pattern programming/Pattern 1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+"""
+*
+* *
+* * *
+* * * *
+* * * * *
+"""
+n=5
+for i in range(n):
+    for j in range(i):
+        print("*",end=" ")
+    print()
---
+++ Pattern programming/Pattern 7.py
@@ -0,0 +1,13 @@
+"""
+2
+2 4
+2 4 6
+2 4 6 8
+2 4 6 8 10
+"""
+
+n=5
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(j*2,end=" ")
+    print()
